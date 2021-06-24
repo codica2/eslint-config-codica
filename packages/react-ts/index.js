9669c048ba8717498216484f29989b83f686ef43
Eugene Volkov <evolkov.codica@gmail.com>
2021-06-24 12:16:31 +0300

    [Fix] naming convention

--- packages/react-ts/index.js
+++ packages/react-ts/index.js
@@ -123,6 +123,21 @@ module.exports = {
         }
       }
     ],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'variable',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE', 'snake_case'],
+      },
+      {
+        selector: 'function',
+        format: ['camelCase', 'PascalCase'],
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+    ],
 
     // React
     'react/jsx-props-no-spreading': 'off',
